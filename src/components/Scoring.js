2faf73775c566dfd5c92c318638538c9e2190095
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2023-05-04 07:29:25 +0800

    minor changes (100%)

--- src/components/Scoring.js
+++ src/components/Scoring.js
@@ -6,6 +6,7 @@ const Scoring = (props) => {
     return (
         <div className="interface">
             <h1>Hello Player.</h1>
+            <h4>Click anywhere to start</h4>
             <div className="interface-right">
                 <h3 style={{margin: 0}}>Your score is {props.score} </h3>
                 <h3 style={{margin: 0}}>Level 1 </h3>
